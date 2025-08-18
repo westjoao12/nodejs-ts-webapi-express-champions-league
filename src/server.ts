d46a7ab0df624782a7dfae553da840b3d99aba01
westjoao12 <westjoao12@gmail.com>
2025-08-18 19:50:38 +0100

    criando o servidor

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) =>{
-    res.send('Hello World!');
+    res.status(200).json({
+        message: 'Welcome to the Champions API!'
+    });
 })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`🔥 Server is running on port ${PORT}`);
 });
