a975a485e015acd8e5b94abbdeee5c654896b8c7
westjoao12 <westjoao12@gmail.com>
2025-08-19 06:54:33 +0100

    responsabilidade a camada de dados

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,31 +1,23 @@
 import { IClubModel } from "../models/interfaces/IClubModel";
-
-const database: IClubModel[] = [
-    {
-        id: 1,
-        name: "Paris Saint-Germain",
-        stadium: "Parc des Princes",
-        manager: "Luíz Henrique"
-    },
-    {
-        id: 2,
-        name: "Manchester United",
-        stadium: "Old Trafford",
-        manager: "Ruben Amorim"
-    },
-    {
-        id: 3,
-        name: "FC Barcelona",
-        stadium: "Camp Nou",
-        manager: "Hansi Flick"
-    }
-];
+import fs from "fs";
 
 export const findAllClubs = async (): Promise<IClubModel[]> => {
+    //const data = fs.readFileSync('src/data/clubs.json', 'utf-8');
+    const data = fs.readFileSync(__dirname + '/../data/clubs.json', 'utf-8');
+    if (!data) {
+        return [];
+    }
+    const database: IClubModel[] = JSON.parse(data);
     return database;
 }
 
 export const findClubById = async (id: number): Promise<IClubModel | null> => {
+    //const data = fs.readFileSync('src/data/clubs.json', 'utf-8');
+    const data = fs.readFileSync(__dirname + '/../data/clubs.json', 'utf-8');
+    if (!data) {
+        return null;
+    }
+    const database: IClubModel[] = JSON.parse(data);
     const club = database.find(c => c.id === id);
     return club || null;
 }
